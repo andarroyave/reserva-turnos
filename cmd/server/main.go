059be417e3ada41587bd8eeb34be9659bf75d98a
Jorge Jimenez <andarroyave@gmail.com>
2023-09-19 13:00:02 -0500

    mod: turns - swagger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"database/sql"
 
+	"github.com/andarroyave/reserva-turnos/cmd/server/handler"
+	"github.com/andarroyave/reserva-turnos/docs"
+	"github.com/andarroyave/reserva-turnos/internal/turn"
+	"github.com/andarroyave/reserva-turnos/pkg/store"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 
-	//"github.com/joho/godotenv"
-	"github.com/andarroyave/reserva-turnos/cmd/server/handler"
-	"github.com/andarroyave/reserva-turnos/internal/turn"
-	"github.com/andarroyave/reserva-turnos/pkg/store"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title PI Reserva Turnos
+// @version 1.0
+// @description This API Handle Products
+// @license.name Apache 2.0
 
 func main() {
 	datasource := "root:password@tcp(localhost:3306)/reserva-turnos"
@@ -28,6 +35,10 @@ func main() {
 	turnHandler := handler.TurnHandler{&turnService}
 
 	server := gin.Default()
+
+	docs.SwaggerInfo.Host = "localhost:8085"
+	server.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	turnServer := server.Group("/turns")
 	turnServer.GET("/:id", turnHandler.GetTurnById)
 	turnServer.POST("/", turnHandler.CreateTurn)
